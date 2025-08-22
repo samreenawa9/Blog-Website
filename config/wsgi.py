f9d30ae8c393a7a8137cdf1239331eee079a7835
Samreen Fatima <samreenfatimamalik@gmail.com>
2025-08-22 15:59:17 +0500

    Updated WSGI file for PythonAnywhere deployment

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/samreenawa9/Blog-Website'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Blog-Website.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
 application = get_wsgi_application()
+
